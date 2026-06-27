7a1a99b49128fdd857df671771ca7788fdb7f89b
hashir500 <hashirhassan319@gmail.com>
2026-06-27 21:41:55 +0500

    visitor_card model

--- highnoon_vms/visitors/admin.py
+++ highnoon_vms/visitors/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import visitor_card
 
 # Register your models here.
+
+admin.site.register(visitor_card)
--- highnoon_vms/visitors/models.py
+++ highnoon_vms/visitors/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class visitor_card(models.Model):
+    card_id = models.AutoField(primary_key= True)
+    card_color = models.CharField(max_length= 50, unique= True)
+    card_access_level= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.card_color
