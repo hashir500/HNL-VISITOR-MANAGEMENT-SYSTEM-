27beae3ef0cc88a01244a8eb61fb2a85f2a3e0a9
hashir500 <hashirhassan319@gmail.com>
2026-06-27 20:59:36 +0500

    department model

--- highnoon_vms/employees/models.py
+++ highnoon_vms/employees/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class department(models.Model):
+    department_id = models.AutoField(primary_key = True)
+    department_name = models.CharField(max_length = 100, unique = True)
+
+    def __str__(self):
+        return self.department_name
