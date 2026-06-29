75572aaeae62fcb36953bd8d8e78bcbe231e758c
hashir500 <hashirhassan319@gmail.com>
2026-06-29 14:50:06 +0500

    sidebar improvement

--- highnoon_vms/system_settings/models.py
+++ highnoon_vms/system_settings/models.py
@@ -6,6 +6,7 @@ class SystemSettings(models.Model):
     company_domain = models.CharField(max_length=150, blank=True, null=True)
     primary_color = models.CharField(max_length=20, default="#cb0c9f")
     secondary_color = models.CharField(max_length=20, default="#8392ab")
+    logo_color = models.CharField(max_length=20, default="#ffffff")
     logo = models.ImageField(upload_to="company_logos/", blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
 
